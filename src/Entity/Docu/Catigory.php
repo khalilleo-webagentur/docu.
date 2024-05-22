810d29ab2042613e0b5a322262c03883e992e975
ronijan <hu@hu.com>
2024-05-22 21:54:11 +0200

    docu. item model, repo and service

--- src/Entity/Docu/Catigory.php
+++ src/Entity/Docu/Catigory.php
@@ -4,6 +4,8 @@ namespace App\Entity\Docu;
 
 use App\Repository\Docu\CatigoryRepository;
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -27,9 +29,16 @@ class Catigory
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $createdAt = null;
 
+    /**
+     * @var Collection<int, Item>
+     */
+    #[ORM\OneToMany(targetEntity: Item::class, mappedBy: 'catigory')]
+    private Collection $items;
+
     public function __construct()
     {
         $this->setCreatedAt(new DateTime());
+        $this->items = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -81,6 +90,36 @@ class Catigory
     public function setCreatedAt(\DateTimeInterface $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Item>
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(Item $item): static
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setCatigory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(Item $item): static
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getCatigory() === $this) {
+                $item->setCatigory(null);
+            }
+        }
 
         return $this;
     }
