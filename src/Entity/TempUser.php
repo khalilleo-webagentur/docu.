9c56a519be25da8ec0d2c35d098cfa9185e5d45e
ronijan <hu@hu.com>
2024-05-26 19:49:43 +0200

    docu. remove storage folder and set tabe prefix

--- src/Entity/TempUser.php
+++ src/Entity/TempUser.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TempUserRepository::class)]
+#[ORM\Table(name: '`docu_temp_user`')]
 class TempUser
 {
     #[ORM\Id]
